64711216f5ba006e588067706c7a942dd2dc24d7
giselleLago <gisellelago25994@gmail.com>
2020-07-17 18:09:58 +0200

    Add project files.

---
+++ CW_Vowel/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_Vowel
+{
+    public class Kata
+    {
+        public int GetVowelCount(string str)
+        {
+            return str.Count(i => "aeiou".Contains(i));
+        }
+    }
+}
---
+++ CW_Vowel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_Vowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.GetVowelCount("Hola"));
+            Console.ReadKey();
+        }
+    }
+}
